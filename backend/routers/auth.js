13007a30e42538e369ff37ddf9a35e6986b6aa5e
Ghina Shour <shourghina@gmail.com>
2025-09-01 21:43:20 +0300

    adding the auth router

---
+++ backend/routers/auth.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { signUp, login } = require('../controllers/authController');
+
+// Signup route
+router.post('/signup', signUp);
+
+// Login route
+router.post('/login', login);
+
+module.exports = router;
